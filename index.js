ea1a02078c21df08b363756a273a8c37fb3b21c8
AHQ Miness <ahqsecret@gmail.com>
2023-06-28 18:46:58 +0530

    fixes

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ module.exports = async function() {
         auth: process.env.token
     });
 
-    const release = await client.rest.repos.createRelease({
-        owner: "ahqsoftwares",
-        repo: "astore-sample-project",
+    const release = await client.request('POST /repos/{owner}/{repo}/releases', {
+        owner: 'ahqsoftwares',
+        repo: 'ahqstore-sample-project',
         tag_name: `v${version}`,
-        name: `AHQ Store Sample App v${version}`,
-        draft: true
+        name: `AHQ Store Sample Project v${version}`,
+        draft: true,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
     });
 
-    release.data.id
-}
+    return release.data.id
+};
