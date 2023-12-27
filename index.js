f56e0c065a96f7e218db28c8952c6bb6b2395c0c
AHQ Miness <ahqsecret@gmail.com>
2023-12-27 20:58:40 +0530

    Fix Fetch

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-module.exports = async function() {
-    const {
-        Octokit
-    } = require("octokit");
+module.exports = (async function () {
+  const { Octokit } = require("octokit");
 
-    const {
-        version
-    } = require("./tauri/package.json");
+  const { version } = require("./tauri/package.json");
 
-    const client = new Octokit({
-        auth: process.env.token
-    });
+  const client = new Octokit({
+    auth: process.env.token,
+  });
 
-    const release = await client.request('POST /repos/{owner}/{repo}/releases', {
-        owner: 'ahqsoftwares',
-        repo: 'ahqstore-sample-project',
-        tag_name: `v${version}`,
-        name: `AHQ Store Sample Project v${version}`,
-        draft: true,
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-        }
-    });
+  const release = await client.request("POST /repos/{owner}/{repo}/releases", {
+    owner: "ahqsoftwares",
+    repo: "ahqstore-sample-project",
+    tag_name: `v${version}`,
+    name: `AHQ Store Sample Project v${version}`,
+    draft: true,
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
 
-    return release.data.id
-}()
+  return release.data.id;
+})();
