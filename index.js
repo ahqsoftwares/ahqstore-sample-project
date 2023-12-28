877582048cc6cbe7fd59fae40472027f9f67b7e9
AHQ Miness <ahqsecret@gmail.com>
2023-12-28 21:44:30 +0530

    New Updates + Update Site

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = (async function () {
+module.exports = async function () {
   const { Octokit } = require("octokit");
 
   const { version } = require("./tauri/package.json");
@@ -16,4 +16,19 @@ module.exports = (async function () {
   });
 
   return release.data.id;
-})();
+};
+
+module.exports.release = async function () {
+  const { Octokit } = require("octokit");
+
+  const client = new Octokit({
+    auth: process.env.token,
+  });
+
+  await client.rest.repos.updateRelease({
+    owner: "ahqstore",
+    repo: "sample-app",
+    release_id: process.env.release_id,
+    draft: false,
+  });
+};
